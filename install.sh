a98c1edb7c3752fe52a64135c78a1aa47ab923c8
gohy907 <gohy2006@mail.ru>
2025-07-04 03:12:08 +0400

    fix: fix zsh install

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 # Zsh
 sudo apt-get install zsh
 sudo chsh -s /bin/zsh $USER
-cp .zshrc ~
+cp zsh/.zshrc ~
 git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp p10k.zsh ~/oh-my-zsh/custom/themes/powerlevel10k/internal
+cp zsh/p10k.zsh ~/.oh-my-zsh/custom/themes/powerlevel10k/internal
 
 # Kitty
 sudo apt-get install kitty
