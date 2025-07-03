0bc88e009f3b0658d9549ecee1d7f1bb24b8d47e
gohy907 <gohy2006@mail.ru>
2025-07-04 03:20:35 +0400

    fix: fix zsh-vi-mode install

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo apt-get install zsh
 sudo chsh -s /bin/zsh $USER
 cp zsh/.zshrc ~
-git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
+git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
