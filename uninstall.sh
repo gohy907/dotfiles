51fb54a70d4a807d859d7decab0d3c8a499574fa
gohy907 <gohy2006@mail.ru>
2025-07-04 13:01:52 +0400

    fix: fix zsh uninstallation

--- uninstall.sh
+++ uninstall.sh
@@ -2,8 +2,9 @@
 
 # Zsh
 sudo apt-get clean zsh
-rm ~/.zshrc
+rm -f ~/.zsh*
 rm -rf ~/.oh-my-zsh
+sudo chsh -s /bin/bash $USER 
 
 # Kitty
 sudo apt-get clean kitty
@@ -17,3 +18,7 @@ rm -rf ~/.local/share/nvim
 
 # Zen 
 rm -rf ~/Downloads/zen.linux-x86_64
+rm -rf ~/.zen
+
+echo
+echo "Please, reboot your computer"
